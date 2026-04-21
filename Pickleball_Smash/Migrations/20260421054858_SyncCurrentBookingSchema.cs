5aa895395afb61f4b825a9ff51e34a7c83a388c4
duy280503 <duynguyen030528@gmail.com>
2026-04-21 12:53:34 +0700

    thêm trường ngày sinh, đổi giờ bắt đầu/kết thúc bằng khung giờ

---
+++ Pickleball_Smash/Migrations/20260421054858_SyncCurrentBookingSchema.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pickleball_Smash.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncCurrentBookingSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ThoiGianBatDau",
+                table: "DON_DAT_SAN");
+
+            migrationBuilder.DropColumn(
+                name: "ThoiGianKetThuc",
+                table: "DON_DAT_SAN");
+
+            migrationBuilder.AddColumn<string>(
+                name: "KhungGio",
+                table: "DON_DAT_SAN",
+                type: "NVARCHAR(255)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KhungGio",
+                table: "DON_DAT_SAN");
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "ThoiGianBatDau",
+                table: "DON_DAT_SAN",
+                type: "time",
+                nullable: true);
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "ThoiGianKetThuc",
+                table: "DON_DAT_SAN",
+                type: "time",
+                nullable: true);
+        }
+    }
+}
